169127d8c4e74d5c24377b175d46b27599e2f4ca
Дмитрий Попков <dmitriypopkov98@gmail.com>
2023-01-17 15:14:30 +0300

    cors

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./configs/database";
 import bodyParser from "body-parser";
 import { requestRouter } from "./routes/requestRouter";
@@ -10,6 +11,8 @@ connectDB();
 const port = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors({ origin: process.env.FRONT_END_URL }));
 
 app.use(bodyParser.json());
 
